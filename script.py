d54fd1b3a523c189b71c550b6d20b3e21f3aa370
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-01-23 20:33:42 -0300

    add code script.py

--- script.py
+++ script.py
@@ -1,13 +1,8 @@
-S = 0
-P = 1
-Q = 2
+with open('Titanic-Dataset.csv', 'r') as f:  # garante que o arquivo será fechado corretamente quando finalizado
+    lines = f.readlines()  # lê todas as linhas do aquivo e armazena na lista 'lines'
+    nomes = [linha.split('"') for linha in lines]
+    dados = [(nomes[c][0].split(','), [nomes[c][1]], nomes[c][2].split(',')) for c in range(1,len(nomes))]
+    final = [c[0] + c[1] +c[2] for c in dados]
 
-S = (0,0)
-P = raiz(3/2)
-Q = (0,1)
-
-
-S
-Q
-
-p
+for line in final:
+  print(line)
