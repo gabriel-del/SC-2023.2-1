1a5cee548c34a31138a5922b27ad8a81ee6855ed
RafaelAlmeida94 <157075613+RafaelAlmeida94@users.noreply.github.com>
2024-01-24 13:14:14 -0300

    script.py

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 final2 = []
 with open('Titanic-Dataset.csv', 'r') as f:  # garante que o arquivo será fechado corretamente quando finalizado
-    lines = f.readlines()  # lê todas as linhas do aquivo e armazena na lista 'lines'
-    nomes = [linha.split('"') for linha in lines]
-    dados = [(nomes[c][0].split(','), [nomes[c][1]], nomes[c][2].split(',')) for c in range(1,len(nomes))]
-    final = [c[0] + c[1] +c[2] for c in dados]
+    lines = f.readlines()  # lê todas as linhas do arquivo e armazena na lista 'lines'
+    nomes = [linha.split('"') for linha in lines] # usa aspas como argumento para separar o nome
+    dados = [(nomes[c][0].split(','), [nomes[c][1]], nomes[c][2].split(',')) for c in range(1,len(nomes))] # usa virgula como argumento para separar o restante dos dados
+    final = [c[0] + c[1] +c[2] for c in dados] # remove as sublistas
 for n in final:
-    if len(n) == 14 and n[7]!= '':
-        lista = [int(n[1]), [int(n[2]), n[6], float(n[7]), int(n[8]), int(n[9]), float(n[11])]]
+    if len(n) == 14 and n[7]!= '': #descarta as listas incompletas e com dados nulos
+        lista = [int(n[1]), [int(n[2]), n[6], float(n[7]), int(n[8]), int(n[9]), float(n[11])]] # monta uma nova lista com os dados essenciais
         if lista[1][1] == 'male': 
             lista[1][1] = 0 # 0 para masculino
         if lista[1][1] == 'female':
             lista[1][1] = 1 #1 para feminino
-        final2.append(lista)
+        final2.append(lista) # lista final
 
 print('Survived,Pclass,Sex,Age,SibSp,Parch,Fare')   
 for n in final2:
