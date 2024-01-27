254d72235a0f1063a0aa85dbb5573a1ff936e0ae
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-01-27 17:10:07 -0300

    rm argument table from function average

--- src/investigation.py
+++ src/investigation.py
@@ -1,17 +1,17 @@
 import sys, os,csv
 sys.path.append(f'{os.path.dirname(__file__)}/../data')
 
-def average(category, table):
-  return sum([float(line[f[0].index(category)]) for line in table]) / len(table)
+def average(category):
+  return sum([float(line[f[0].index(category)]) for line in Survived_1]) / len(Survived_1)
 
 with open(f'{os.path.dirname(__file__)}/../data/Titanic-Processed.csv', 'r') as f:
   f = list(csv.reader(f, delimiter=',', lineterminator='\n'))
   Survived_1 = list(filter(lambda x: x[1] == '1', f))
 
-  print(f"Pclass: {average('Pclass', Survived_1)}")
-  print(f"Sex: {average('Sex', Survived_1)}")
-  print(f"Age: {average('Age', Survived_1)}")
-  print(f"SibSp: {average('SibSp', Survived_1)}")
-  print(f"Parch: {average('Parch', Survived_1)}")
-  print(f"Fare: {average('Fare', Survived_1)}")
-  # print(f"Embarked: {average('Embarked', Survived_1)}")
+  print(f"Pclass: {average('Pclass')}")
+  print(f"Sex: {average('Sex')}")
+  print(f"Age: {average('Age')}")
+  print(f"SibSp: {average('SibSp')}")
+  print(f"Parch: {average('Parch')}")
+  print(f"Fare: {average('Fare')}")
+  # print(f"Embarked: {average('Embarked')}")
