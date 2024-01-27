9e1723104226df1bef35a23011cd8729d141e2d8
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-01-27 10:01:44 -0300

    cut table

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ from knn import *
 
 
 with open(f'{os.path.dirname(__file__)}/../data/Titanic-Processed.csv', 'r') as f:  # lê e fecha arquivo
+    f = list(csv.reader(f, delimiter=',', lineterminator='\n'))
+    # knn = KNNClass(f)
+    id = [line[0] for line in f[1:]]
+    target = [line[1] for line in f[1:]]
+    data = [line[2:] for line in f[1:]]
+    print(data)
