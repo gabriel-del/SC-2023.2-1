a8c8804a1c392da27b1be02317fac3d849faa62a
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-01-28 16:20:51 -0300

    main.py show statistics

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ import sys, os, csv
 sys.path.append(f'{os.path.dirname(__file__)}/../data')
 from knn import *
 
-
+accuracies = []
 with open(f'{os.path.dirname(__file__)}/../data/Titanic-Processed.csv', 'r') as f:
     f = list(csv.reader(f, delimiter=',', lineterminator='\n'))
-    knn = KNNClass(f)
-    # print(list(knn.calculate_distances(['0', '0', '0.325', '0.0', '0.0', '0.030022053231939166', '1']))[:10])
-    knn.predict()
-    print(knn.accuracy())
+    for _ in range(100):
+        knn = KNNClass(f)
+        # print(list(knn.calculate_distances(['0', '0', '0.325', '0.0', '0.0', '0.030022053231939166', '1']))[:10])
+        knn.predict()
+        accuracy = knn.accuracy()
+        print(accuracy)
+        accuracies.append(accuracy)
+
+print('\nAcurácias: ')
+print(f'Média: {sum(accuracies) / len(accuracies)}, máxima: {max(accuracies)}, mínima: {min(accuracies)}')
